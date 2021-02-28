611d234f1c26f247cdc9f8cb4ff24913b91d3ab5
crowe828 <69489498+crowe828@users.noreply.github.com>
2021-02-28 13:28:58 -0500

    Testing pushing from Eclipse

--- FileReader/src/FileReader.java
+++ FileReader/src/FileReader.java
@@ -1,9 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class FileReader {
 
-	public static void main(String[] args) {
-		System.out.println("hello");
-
+	public static void main(String[] args) throws IOException {
+		File file = new File("/Users/christianrowe/Documents/Example Projects/captmidn.txt");
+		Scanner scan = new Scanner(file);
+		
+		String fileContent = "TESTY TEST TEST";
+		while (scan.hasNextLine()) {
+			fileContent = fileContent.concat(scan.nextLine() + "\n");
+//			System.out.println(scan.nextLine());
+		}
+		
+		FileWriter writer = new FileWriter("/Users/christianrowe/Documents/Example Projects/newfile.txt");
+		writer.write(fileContent);
+		writer.close();
 	}
 
 }
